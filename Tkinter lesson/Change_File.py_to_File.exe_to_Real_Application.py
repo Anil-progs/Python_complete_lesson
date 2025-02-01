91d6563d9766547dea36151cfce11e514e929683
Anil Amiri 2 <anilamiri2005@gmail.com>
2025-01-31 18:45:41 -0800

    Add files via upload

---
+++ Tkinter lesson/Change_File.py_to_File.exe_to_Real_Application.py
@@ -0,0 +1,12 @@
+first create a sparate file for it and open the cmd
+
+cd desktop
+cd App
+pip install pyinstaller 
+
+
+pyinstaller main.py  #only able to run on that file
+
+pyinstaller main.py -onefile    #you can run it in all the place but it has black board in background of itself
+
+pyinstaller main.py --onefile -w   #you can run it in all the place and it do not show a black board in back of itself
